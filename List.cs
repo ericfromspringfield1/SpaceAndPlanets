60ad035e98c9a9d3ba2006696f0b0b3ab7c2ab6c
Eric Taylor <ericfromspringfield@gmail.com>
2019-10-04 15:29:02 -0500

    Planets and Stuff

---
+++ List.cs
@@ -0,0 +1,6 @@
+namespace lists
+{
+    internal class List<T1, T2>
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> planetList2 = new List<string>() { "Neptune", "Uranus" };
+
+
+            planetList.AddRange(planetList2);
+            planetList.Insert(3, "Earth");
+            planetList.Insert(4, "Venus");
+            planetList.Add("Pluto");
+
+            Console.WriteLine("-------Rocky Plizzies------");
+            List<string> rockyPlanets = planetList.GetRange(0, 5);
+
+            foreach (string planet in rockyPlanets)
+            {
+
+                Console.WriteLine(planet);
+            }
+        planetList.Remove("Pluto");
+        foreach (string planet in planetList) 
+        {
+            Console.WriteLine(planet);
+        }
+        }
+    }
+}
+    
